199dce91a06a161634ed0f7545a331f41b1505ab
jeebzi <jeannguyen033@gmail.com>
2026-02-20 14:27:02 +0100

    changé makefile pour prendre en compte variable d'environement et commencé un petit fichier LaTeX

--- popcount_test/sans.s
+++ popcount_test/sans.s
@@ -1,53 +1,435 @@
 	.file	"main.c"
 	.intel_syntax noprefix
 	.text
+.Ltext0:
+	.file 0 "/home/imath-stage1/jb/nonlinearity/popcount_test" "main.c"
 	.globl	__popcountdi2
-	.section	.rodata.str1.1,"aMS",@progbits,1
+	.section	.rodata
 .LC0:
 	.string	"%d\n"
-	.section	.text.startup,"ax",@progbits
-	.p2align 4
+	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB39:
+.LFB6:
+	.file 1 "main.c"
+	.loc 1 9 34
 	.cfi_startproc
 	endbr64
-	sub	rsp, 8
+	push	rbp
 	.cfi_def_cfa_offset 16
-	mov	rdi, QWORD PTR 16[rsi]
-	mov	edx, 10
-	xor	esi, esi
-	call	strtol@PLT
-	mov	edi, eax
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 32
+	mov	DWORD PTR -20[rbp], edi
+	mov	QWORD PTR -32[rbp], rsi
+	.loc 1 11 36
+	mov	rax, QWORD PTR -32[rbp]
+	add	rax, 16
+	.loc 1 11 27
+	mov	rax, QWORD PTR [rax]
+	mov	rdi, rax
+	call	atoi@PLT
+	.loc 1 11 8
+	mov	eax, eax
+	mov	rdi, rax
 	call	__popcountdi2@PLT
-	lea	rsi, .LC0[rip]
-	mov	edi, 1
-	mov	edx, eax
-	xor	eax, eax
-	call	__printf_chk@PLT
-	xor	eax, eax
-	add	rsp, 8
-	.cfi_def_cfa_offset 8
+	mov	DWORD PTR -4[rbp], eax
+	.loc 1 12 2
+	mov	eax, DWORD PTR -4[rbp]
+	mov	esi, eax
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	.loc 1 13 9
+	mov	eax, 0
+	.loc 1 14 1
+	leave
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE39:
+.LFE6:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
+.Letext0:
+	.file 2 "/usr/include/stdio.h"
+	.file 3 "/usr/include/stdlib.h"
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.long	0x106
+	.value	0x5
+	.byte	0x1
+	.byte	0x8
+	.long	.Ldebug_abbrev0
+	.uleb128 0x5
+	.long	.LASF16
+	.byte	0x1d
+	.long	.LASF0
+	.long	.LASF1
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.long	.Ldebug_line0
+	.uleb128 0x1
+	.byte	0x8
+	.byte	0x7
+	.long	.LASF2
+	.uleb128 0x6
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.uleb128 0x1
+	.byte	0x8
+	.byte	0x5
+	.long	.LASF3
+	.uleb128 0x1
+	.byte	0x8
+	.byte	0x5
+	.long	.LASF4
+	.uleb128 0x1
+	.byte	0x1
+	.byte	0x8
+	.long	.LASF5
+	.uleb128 0x1
+	.byte	0x2
+	.byte	0x7
+	.long	.LASF6
+	.uleb128 0x1
+	.byte	0x4
+	.byte	0x7
+	.long	.LASF7
+	.uleb128 0x1
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF8
+	.uleb128 0x1
+	.byte	0x2
+	.byte	0x5
+	.long	.LASF9
+	.uleb128 0x2
+	.long	0x72
+	.uleb128 0x1
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF10
+	.uleb128 0x7
+	.long	0x72
+	.uleb128 0x1
+	.byte	0x8
+	.byte	0x7
+	.long	.LASF11
+	.uleb128 0x8
+	.long	.LASF12
+	.byte	0x2
+	.value	0x164
+	.byte	0xc
+	.long	0x35
+	.long	0x9d
+	.uleb128 0x3
+	.long	0x9d
+	.uleb128 0x9
+	.byte	0
+	.uleb128 0x2
+	.long	0x79
+	.uleb128 0xa
+	.long	.LASF13
+	.byte	0x3
+	.byte	0x69
+	.byte	0xc
+	.long	0x35
+	.long	0xb8
+	.uleb128 0x3
+	.long	0x9d
+	.byte	0
+	.uleb128 0xb
+	.long	.LASF17
+	.byte	0x1
+	.byte	0x9
+	.byte	0x5
+	.long	0x35
+	.quad	.LFB6
+	.quad	.LFE6-.LFB6
+	.uleb128 0x1
+	.byte	0x9c
+	.long	0x104
+	.uleb128 0x4
+	.long	.LASF14
+	.byte	0xe
+	.long	0x35
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -36
+	.uleb128 0x4
+	.long	.LASF15
+	.byte	0x1a
+	.long	0x104
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -48
+	.uleb128 0xc
+	.string	"res"
+	.byte	0x1
+	.byte	0xa
+	.byte	0x6
+	.long	0x35
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -20
+	.byte	0
+	.uleb128 0x2
+	.long	0x6d
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0xf
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0x21
+	.sleb128 8
+	.uleb128 0x49
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x5
+	.byte	0
+	.uleb128 0x49
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x4
+	.uleb128 0x5
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0x21
+	.sleb128 1
+	.uleb128 0x3b
+	.uleb128 0x21
+	.sleb128 9
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2
+	.uleb128 0x18
+	.byte	0
+	.byte	0
+	.uleb128 0x5
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x1f
+	.uleb128 0x1b
+	.uleb128 0x1f
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x7
+	.uleb128 0x10
+	.uleb128 0x17
+	.byte	0
+	.byte	0
+	.uleb128 0x6
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.byte	0
+	.byte	0
+	.uleb128 0x7
+	.uleb128 0x26
+	.byte	0
+	.uleb128 0x49
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x8
+	.uleb128 0x2e
+	.byte	0x1
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0x5
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x27
+	.uleb128 0x19
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x3c
+	.uleb128 0x19
+	.uleb128 0x1
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x9
+	.uleb128 0x18
+	.byte	0
+	.byte	0
+	.byte	0
+	.uleb128 0xa
+	.uleb128 0x2e
+	.byte	0x1
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x27
+	.uleb128 0x19
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x3c
+	.uleb128 0x19
+	.uleb128 0x1
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0x2e
+	.byte	0x1
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x27
+	.uleb128 0x19
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x7
+	.uleb128 0x40
+	.uleb128 0x18
+	.uleb128 0x7c
+	.uleb128 0x19
+	.uleb128 0x1
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0xc
+	.uleb128 0x34
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x8
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2
+	.uleb128 0x18
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.long	0x2c
+	.value	0x2
+	.long	.Ldebug_info0
+	.byte	0x8
+	.byte	0
+	.value	0
+	.value	0
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.quad	0
+	.quad	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF4:
+	.string	"long long int"
+.LASF7:
+	.string	"unsigned int"
+.LASF17:
+	.string	"main"
+.LASF2:
+	.string	"long unsigned int"
+.LASF11:
+	.string	"long long unsigned int"
+.LASF5:
+	.string	"unsigned char"
+.LASF10:
+	.string	"char"
+.LASF3:
+	.string	"long int"
+.LASF13:
+	.string	"atoi"
+.LASF14:
+	.string	"argc"
+.LASF6:
+	.string	"short unsigned int"
+.LASF12:
+	.string	"printf"
+.LASF15:
+	.string	"argv"
+.LASF9:
+	.string	"short int"
+.LASF16:
+	.string	"GNU C17 11.4.0 -masm=intel -mtune=generic -march=x86-64 -g -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection"
+.LASF8:
+	.string	"signed char"
+	.section	.debug_line_str,"MS",@progbits,1
+.LASF1:
+	.string	"/home/imath-stage1/jb/nonlinearity/popcount_test"
+.LASF0:
+	.string	"main.c"
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04.2) 11.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
 	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
 0:
-	.string	 "GNU"
+	.string	"GNU"
 1:
 	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
+	.long	0xc0000002
+	.long	3f - 2f
 2:
-	.long	 0x3
+	.long	0x3
 3:
 	.align 8
 4:
